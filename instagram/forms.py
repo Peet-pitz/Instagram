1e2a9b171ebc4bab5ed240e10fa4005a382e3401
Peter Njoroge <njoroge.pitz@gmail.com>
2018-11-12 16:22:58 +0300

    create new profile form

--- instagram/forms.py
+++ instagram/forms.py
@@ -6,10 +6,10 @@ class NewImageForm(forms.ModelForm):
         model = Image
         exclude = ['profile', 'photo_date','likes','comment','image_name']
 
-# class NewProfileForm(forms.ModelForm):
-#     class Meta:
-#         model = Profile
-#         exclude = ['user']
+class NewProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        exclude = ['user']
 
 # class NewCommentForm(forms.ModelForm):
 #     class Meta :
