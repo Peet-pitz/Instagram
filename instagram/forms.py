dc803e700ac447697e3b0215cbaf22a991586e33
Peter Njoroge <njoroge.pitz@gmail.com>
2018-11-12 16:22:40 +0300

    create new image form

--- instagram/forms.py
+++ instagram/forms.py
@@ -1,10 +1,10 @@
-# from django import forms
-# from .models import Image,Profile,Comment
+from django import forms
+from .models import Image,Profile,Comment
 
-# class NewImageForm(forms.ModelForm):
-#     class Meta:
-#         model = Image
-#         exclude = ['profile', 'photo_date','likes','comment','image_name']
+class NewImageForm(forms.ModelForm):
+    class Meta:
+        model = Image
+        exclude = ['profile', 'photo_date','likes','comment','image_name']
 
 # class NewProfileForm(forms.ModelForm):
 #     class Meta:
