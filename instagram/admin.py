77202df4a8ba108307236cc1b658f5fa055cf39b
Peter Njoroge <njoroge.pitz@gmail.com>
2018-11-12 16:10:29 +0300

    create Instagram app

---
+++ instagram/admin.py
@@ -0,0 +1,11 @@
+# from django.contrib import admin
+# from .models import Image,Profile,Comment
+# # Register your models here.
+# class ImageAdmin(admin.ModelAdmin):
+#     filter_horizontal =('profile',)
+
+# admin.site.register(Image)
+
+# admin.site.register(Profile)
+
+# admin.site.register(Comment)
