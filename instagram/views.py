cfeb5b659500d63b38b725354a51c6fba5a99253
Peter Njoroge <njoroge.pitz@gmail.com>
2018-11-12 16:13:52 +0300

    create new image function

--- instagram/views.py
+++ instagram/views.py
@@ -32,19 +32,19 @@ def image(request,image_id):
         raise Http404()
     return render(request,'all-photos/image.html',{'image':image})
 
-# @login_required(login_url='/accounts/login/')
-# def new_image(request):
-#     current_user = request.user
-#     if request.method == 'POST':
-#         form = NewImageForm(request.POST, request.FILES)
-#         if form.is_valid():
-#             image = form.save(commit = False)
-#             image.profile = current_user
-#             image.save()
-#         return redirect('instaToday')
-#     else:
-#         form = NewImageForm()
-#     return render(request,'new_image.html', {'form':form})
+@login_required(login_url='/accounts/login/')
+def new_image(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = NewImageForm(request.POST, request.FILES)
+        if form.is_valid():
+            image = form.save(commit = False)
+            image.profile = current_user
+            image.save()
+        return redirect('instaToday')
+    else:
+        form = NewImageForm()
+    return render(request,'new_image.html', {'form':form})
 
 # def profile(request):
 #     current_user = request.user
